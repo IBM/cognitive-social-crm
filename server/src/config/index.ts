401284f25af8931de3c017f006f119f4cb54db68
Sanjeev Ghimire <sanjeev.ghimire@ibm.com>
2018-08-31 16:39:48 -0500

    dependency cleanup and cf push

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 // tslint:disable-next-line:no-var-requires
 const vcapServices = require('vcap_services');
-const cloudantCreds = vcapServices.getCredentials('cloudantNoSQLDB');
+
 const ANALYSIS_DB: string = 'analysis_db';
 
 export const ENV = {
@@ -11,28 +11,14 @@ export const ENV = {
   test: 'testing',
 };
 
-// tslint:disable:variable-name
-let cloudantUsername: string;
-let cloudantPassword: string;
-let cloudantDB: string;
-if (process.env.NODE_ENV === 'production') {
-  cloudantUsername = cloudantCreds.username;
-  cloudantPassword = cloudantCreds.password;
-  cloudantDB = ANALYSIS_DB;
-} else {
-  cloudantUsername = process.env.CLOUDANT_USERNAME || '';
-  cloudantPassword = process.env.CLOUDANT_PASSWORD || '';
-  cloudantDB = process.env.CLOUDANT_ANALYSIS_DB_NAME || '';
-}
-
 let config = {
   environment: process.env.NODE_ENV || ENV.dev,
   port: process.env.PORT || 3000,
   logging: process.env.LOGGING,
   log_level: process.env.LOGLEVEL,
-  cloudant_username: cloudantUsername,
-  cloudant_password: cloudantPassword,
-  cloudant_db: cloudantDB,
+  cloudant_username: process.env.CLOUDANT_USERNAME,
+  cloudant_password: process.env.CLOUDANT_PASSWORD,
+  cloudant_db: process.env.CLOUDANT_ANALYSIS_DB_NAME,
   listenFor: process.env.TWITTER_LISTEN_FOR,
   listenTo: process.env.TWITTER_LISTEN_TO,
   filterContaining: process.env.TWITTER_FILTER_CONTAINING,
@@ -44,8 +30,19 @@ let config = {
   access_token_secret: process.env.TWITTER_ACCESS_SECRET,
   max_buffer_size: Number(process.env.MAX_BUFFER_SIZE),
   isLocal: true,
-
 };
+
+if (process.env.NODE_ENV === 'production') {
+  const cloudantCreds = vcapServices.getCredentials('cloudantNoSQLDB');
+  if(!cloudantCreds){
+    console.log("Couldn't find `vcap_services` to read credentials from.");
+  }
+  config.cloudant_username = cloudantCreds.username;
+  config.cloudant_password = cloudantCreds.password;
+  config.cloudant_db = ANALYSIS_DB;
+}
+
+
 
 // merge environment specific config to default config.
 // tslint:disable-next-line:no-var-requires
